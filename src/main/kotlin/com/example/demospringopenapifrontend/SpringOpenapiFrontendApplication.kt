7c48d5ef125386ae11f71b1db20790c6530a1931
krasowskiM <krasowski.m87@gmail.com>
2022-03-05 16:41:23 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/demospringopenapifrontend/SpringOpenapiFrontendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demospringopenapifrontend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringOpenapiFrontendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringOpenapiFrontendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demospringopenapifrontend/SpringOpenapiFrontendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demospringopenapifrontend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringOpenapiFrontendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
